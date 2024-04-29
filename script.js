00effee2674ca9909df4c20d8e04d996151b7ba9
Bruno Segovia <brunosegovia2009@gmail.com>
2024-04-29 10:15:50 -0300

    cambios Bruno

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const menuToggle = document.querySelector('.menu-toggle');
     const navbarMenu = document.querySelector('.navbar-menu');
-    menuContainer = document.querySelector('.menu-container');
+    const menuContainer = document.querySelector('.menu-container');
     const links = document.querySelectorAll('.smooth-scroll');
 
     menuToggle.addEventListener('click', function() {
@@ -25,39 +25,46 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-    // Cerrar el menú desplegable cuando se hace clic en un enlace
+    // Cambiar a las páginas correspondientes cuando se hace clic en los enlaces
     links.forEach(link => {
         link.addEventListener('click', function(e) {
             e.preventDefault(); // Evitar el comportamiento predeterminado del enlace
-            const targetId = this.getAttribute('href');
-            const targetPosition = document.querySelector(targetId).offsetTop;
-            const startPosition = window.pageYOffset;
-            const distance = targetPosition - startPosition;
-            const duration = 1000; // Duración del desplazamiento en milisegundos
-            let start = null;
+            const targetUrl = this.getAttribute('href');
+            if (targetUrl.charAt(0) === '#') {
+                // Close the menu before smooth scroll
+                navbarMenu.classList.remove('active');
+                menuContainer.classList.remove('active');
+                
+                // Smooth scroll to target element
+                const targetElement = document.querySelector(targetUrl);
+                if (targetElement) {
+                    const targetPosition = targetElement.offsetTop;
+                    const startPosition = window.pageYOffset;
+                    const distance = targetPosition - startPosition;
+                    const duration = 1000; // Duración del desplazamiento en milisegundos
+                    let start = null;
 
-            window.requestAnimationFrame(step);
+                    window.requestAnimationFrame(step);
 
-            function step(timestamp) {
-                if (!start) start = timestamp;
-                const progress = timestamp - start;
-                window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
-                if (progress < duration) window.requestAnimationFrame(step);
+                    function step(timestamp) {
+                        if (!start) start = timestamp;
+                        const progress = timestamp - start;
+                        window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
+                        if (progress < duration) window.requestAnimationFrame(step);
+                    }
+
+                    // Easing function
+                    function easeInOutCubic(t, b, c, d) {
+                        t /= d / 2;
+                        if (t < 1) return c / 2 * t * t * t + b;
+                        t -= 2;
+                        return c / 2 * (t * t * t + 2) + b;
+                    }
+                }
+            } else {
+                // Redirect to target URL
+                window.location.href = targetUrl;
             }
-
-            // Easing function
-            function easeInOutCubic(t, b, c, d) {
-                t /= d / 2;
-                if (t < 1) return c / 2 * t * t * t + b;
-                t -= 2;
-                return c / 2 * (t * t * t + 2) + b;
-            }
-
-            // Cerrar el menú desplegable después de un breve retraso para permitir que termine la animación de desplazamiento suave
-            setTimeout(() => {
-                navbarMenu.classList.remove('active'); // Remover la clase 'active' del menú desplegable
-                menuContainer.classList.remove('active'); // Remover la clase 'active' del contenedor del menú
-            }, duration);
         });
     });
 
